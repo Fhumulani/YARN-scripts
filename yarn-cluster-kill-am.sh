f0f8ea01459c09e43a2ae1c8f078e9b0bfc387fe
Nemulodi <fhumun@147ddae1dd78.ant.amazon.com>
2023-01-30 22:09:39 +0200

    first push

---
+++ yarn-cluster-kill-am.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+aws emr create-cluster \
+ --name "Kill Containers" \
+ --log-uri "s3n://aws-emr-resources-082810018603-us-east-1/" \
+ --release-label "emr-5.36.0" \
+ --service-role "EMR_DefaultRole" \
+ --ec2-attributes '{"InstanceProfile":"EMR_EC2_DefaultRole","EmrManagedMasterSecurityGroup":"sg-0c30ec02dcdbea612","EmrManagedSlaveSecurityGroup":"sg-0774dfd0b14ecedf5","KeyName":"hadoop","AdditionalMasterSecurityGroups":[],"AdditionalSlaveSecurityGroups":[],"SubnetId":"subnet-02e679d6e33c5fba3"}' \
+ --applications Name=Hadoop Name=Hive Name=Spark \
+ --configurations '[{"Classification":"hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}},{"Classification":"spark-hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}}]' \
+ --instance-groups '[{"InstanceCount":3,"InstanceGroupType":"CORE","Name":"Core","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}},{"InstanceCount":1,"InstanceGroupType":"MASTER","Name":"Primary","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}}]' \
+ --steps '[{"Name":"Kill AM","ActionOnFailure":"CONTINUE","Jar":"s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar","Properties":"","Args":["s3://fhumustepvigi/yarn-kill-container.sh","hadoop.pem"]}]' --scale-down-behavior "TERMINATE_AT_TASK_COMPLETION" --ebs-root-volume-size "30" \
+--auto-termination-policy '{"IdleTimeout":600}' \
+--step-concurrency-level "2" \
+--os-release-label "2.0.20221210.1" \
+--region "us-east-1"
+
+
+aws emr create-cluster \
+ --name "STOP RM" \
+ --log-uri "s3n://aws-emr-resources-082810018603-us-east-1/" \
+ --release-label "emr-5.36.0" \
+ --service-role "EMR_DefaultRole" \
+ --ec2-attributes '{"InstanceProfile":"EMR_EC2_DefaultRole","EmrManagedMasterSecurityGroup":"sg-0c30ec02dcdbea612","EmrManagedSlaveSecurityGroup":"sg-0774dfd0b14ecedf5","KeyName":"hadoop","AdditionalMasterSecurityGroups":[],"AdditionalSlaveSecurityGroups":[],"SubnetId":"subnet-02e679d6e33c5fba3"}' \
+ --applications Name=Hadoop Name=Hive Name=Spark \
+ --configurations '[{"Classification":"hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}},{"Classification":"spark-hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}}]' \
+ --instance-groups '[{"InstanceCount":3,"InstanceGroupType":"CORE","Name":"Core","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}},{"InstanceCount":1,"InstanceGroupType":"MASTER","Name":"Primary","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}}]' \
+ --steps '[{"Name":"Kill AM","ActionOnFailure":"CONTINUE","Jar":"s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar","Properties":"","Args":["s3://fhumustepvigi/yarn-stop-RM.sh","hadoop.pem"]}]' --scale-down-behavior "TERMINATE_AT_TASK_COMPLETION" --ebs-root-volume-size "30" \
+--auto-termination-policy '{"IdleTimeout":600}' \
+--step-concurrency-level "2" \
+--os-release-label "2.0.20221210.1" \
+--region "us-east-1"
+
+
+aws emr create-cluster \
+ --name "KILL AM" \
+ --log-uri "s3n://aws-emr-resources-082810018603-us-east-1/" \
+ --release-label "emr-5.36.0" \
+ --service-role "EMR_DefaultRole" \
+ --ec2-attributes '{"InstanceProfile":"EMR_EC2_DefaultRole","EmrManagedMasterSecurityGroup":"sg-0c30ec02dcdbea612","EmrManagedSlaveSecurityGroup":"sg-0774dfd0b14ecedf5","KeyName":"hadoop","AdditionalMasterSecurityGroups":[],"AdditionalSlaveSecurityGroups":[],"SubnetId":"subnet-02e679d6e33c5fba3"}' \
+ --applications Name=Hadoop Name=Hive Name=Spark \
+ --configurations '[{"Classification":"hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}},{"Classification":"spark-hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}}]' \
+ --instance-groups '[{"InstanceCount":3,"InstanceGroupType":"CORE","Name":"Core","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}},{"InstanceCount":1,"InstanceGroupType":"MASTER","Name":"Primary","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}}]' \
+ --steps '[{"Name":"Kill AM","ActionOnFailure":"CONTINUE","Jar":"s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar","Properties":"","Args":["s3://fhumustepvigi/yarn-kill-am.sh","hadoop.pem"]}]' --scale-down-behavior "TERMINATE_AT_TASK_COMPLETION" --ebs-root-volume-size "30" \
+--auto-termination-policy '{"IdleTimeout":600}' \
+--step-concurrency-level "2" \
+--os-release-label "2.0.20221210.1" \
+--region "us-east-1"
+
+
+aws emr create-cluster \
+ --name "STOP NM AM" \
+ --log-uri "s3n://aws-emr-resources-082810018603-us-east-1/" \
+ --release-label "emr-5.36.0" \
+ --service-role "EMR_DefaultRole" \
+ --ec2-attributes '{"InstanceProfile":"EMR_EC2_DefaultRole","EmrManagedMasterSecurityGroup":"sg-0c30ec02dcdbea612","EmrManagedSlaveSecurityGroup":"sg-0774dfd0b14ecedf5","KeyName":"hadoop","AdditionalMasterSecurityGroups":[],"AdditionalSlaveSecurityGroups":[],"SubnetId":"subnet-02e679d6e33c5fba3"}' \
+ --applications Name=Hadoop Name=Hive Name=Spark \
+ --configurations '[{"Classification":"hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}},{"Classification":"spark-hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}}]' \
+ --instance-groups '[{"InstanceCount":3,"InstanceGroupType":"CORE","Name":"Core","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}},{"InstanceCount":1,"InstanceGroupType":"MASTER","Name":"Primary","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}}]' \
+ --steps '[{"Name":"Kill AM","ActionOnFailure":"CONTINUE","Jar":"s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar","Properties":"","Args":["s3://fhumustepvigi/yarn-stop-nm-am-node.sh","hadoop.pem"]}]' --scale-down-behavior "TERMINATE_AT_TASK_COMPLETION" --ebs-root-volume-size "30" \
+--auto-termination-policy '{"IdleTimeout":600}' \
+--step-concurrency-level "2" \
+--os-release-label "2.0.20221210.1" \
+--region "us-east-1"
+
+
+aws emr create-cluster \
+ --name "REBOOT AM NODE" \
+ --log-uri "s3n://aws-emr-resources-082810018603-us-east-1/" \
+ --release-label "emr-5.36.0" \
+ --service-role "EMR_DefaultRole" \
+ --ec2-attributes '{"InstanceProfile":"EMR_EC2_DefaultRole","EmrManagedMasterSecurityGroup":"sg-0c30ec02dcdbea612","EmrManagedSlaveSecurityGroup":"sg-0774dfd0b14ecedf5","KeyName":"hadoop","AdditionalMasterSecurityGroups":[],"AdditionalSlaveSecurityGroups":[],"SubnetId":"subnet-02e679d6e33c5fba3"}' \
+ --applications Name=Hadoop Name=Hive Name=Spark \
+ --configurations '[{"Classification":"hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}},{"Classification":"spark-hive-site","Properties":{"hive.metastore.client.factory.class":"com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"}}]' \
+ --instance-groups '[{"InstanceCount":3,"InstanceGroupType":"CORE","Name":"Core","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}},{"InstanceCount":1,"InstanceGroupType":"MASTER","Name":"Primary","InstanceType":"m5.xlarge","EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"VolumeType":"gp2","SizeInGB":32},"VolumesPerInstance":2}]}}]' \
+ --steps '[{"Name":"Kill AM","ActionOnFailure":"CONTINUE","Jar":"s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar","Properties":"","Args":["s3://fhumustepvigi/yarn-reboot-am-node.sh","hadoop.pem"]}]' --scale-down-behavior "TERMINATE_AT_TASK_COMPLETION" --ebs-root-volume-size "30" \
+--auto-termination-policy '{"IdleTimeout":600}' \
+--step-concurrency-level "2" \
+--os-release-label "2.0.20221210.1" \
+--region "us-east-1"
---
+++ yarn-stop-nm-am-node.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CLUSTER_ID=$(jq -r .jobFlowId /emr/instance-controller/lib/info/job-flow.json)
+
+
+#submit a jobi
+
+keypair=$1
+
+aws s3 cp s3://fhumustepvigi/$keypair /home/hadoop/
+chmod 400 /home/hadoop/$keypair
+
+aws emr add-steps --cluster-id $CLUSTER_ID \
+--steps '[{"Args":["spark-submit","--deploy-mode","cluster","--driver-memory","8G","s3://fhumustepvigi/chow-yarn-whole.py"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"Stop NM on AM Node"}]'
+
+ 
+sleep 240
+
+AM_ID=$(yarn application -list | grep application_167)
+
+echo $AM_ID
+
+aws emr list-instances --cluster-id $CLUSTER_ID | grep PrivateDnsName | awk -F'"' '{print $4}' > /home/hadoop/ips.txt
+
+input=/home/hadoop/ips.txt
+
+while IFS= read -r ip
+do
+    if [[ $AM_ID == *"$ip"* ]];
+    then
+      echo "#!/bin/bash" > /home/hadoop/worker.sh
+      echo "sudo systemctl stop hadoop-yarn-nodemanager.service" >> /home/hadoop/worker.sh
+      ssh -o StrictHostKeyChecking=no -i /home/hadoop/$keypair hadoop@$ip 'bash -s' <  /home/hadoop/worker.sh       
+    fi
+done < "$input"
+
+yarn application -list
+
+chmod 777 /home/hadoop/$keypair
+rm /home/hadoop/$keypair
